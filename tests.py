b545b0329d98bf99a137af37127e8ee63f76f16f
Kevin <etienne.kevin@gmail.com>
2011-10-18 18:32:25 +0100

    added first unit test

--- tests.py
+++ tests.py
@@ -1,6 +1,16 @@
 import unittest
 from parser import Svg
 
+filename = 'test.svg'
+
 class SvgTest(unittest.TestCase):
     def setUp(self):
-        pass
+        self.svg = Svg()
+        self.svg_parsed = self.svg.parse(filename)
+
+    def test_data_parsed(self):
+        self.assertTrue('svg' in self.svg.get_item_by_name('svg'),
+                'should return an svg element')
+
+if __name__ == "__main__":
+    unittest.main()
