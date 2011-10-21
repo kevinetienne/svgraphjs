d3d2a009ac28fad213ff37630a2e7fbcdbf384c3
ekevin <etienne.kevin@gmail.com>
2011-10-22 01:02:15 +0200

    fixed typo for a token

--- tokens.py
+++ tokens.py
@@ -1,7 +1,7 @@
 # init a js variable
 init_var_with_value = "var %s = %s;"
 # create a rectangle element with raphaeljs
-raphael_rect = "paper.rect(%(x)s,%(y)s,%(width)s,%(height)s)i"
+raphael_rect = "paper.rect(%(x)s,%(y)s,%(width)s,%(height)s);"
 # create an ellipse element with raphaeljs
 raphael_ellipse = "paper.ellipse(%(cx)s,%(cy)s,%(rx)s,%(ry)s);"
 # define attributes for an element
