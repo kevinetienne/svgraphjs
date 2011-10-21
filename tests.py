cf85ce743e71e510b08f496fe9903ed44f2e5d64
ekevin <etienne.kevin@gmail.com>
2011-10-21 14:56:08 +0100

    changed var name

--- tests.py
+++ tests.py
@@ -3,20 +3,20 @@ from parser import SVGParser
 
 filename = 'test.svg'
 
-class SvgTest(unittest.TestCase):
+class SVGParserTest(unittest.TestCase):
     def setUp(self):
-        self.svg = SVGParser()
-        self.svg_parsed = self.svg.parse(filename)
+        self.svg_parser = SVGParser()
+        self.svg_parsed = self.svg_parser.parse(filename)
 
     def test_data_parsed(self):
-        self.assertTrue('svg' in self.svg.element,
+        self.assertTrue('svg' in self.svg_parser.element,
                 'should return an svg element')
 
     def test_normalize(self):
         uri = "http://example.com/tags"
         tag = "tag"
         namespaced_element = "{%s}%s" % (uri, tag)
-        normalized_uri, normalized_tag = self.svg._normalize(namespaced_element)
+        normalized_uri, normalized_tag = self.svg_parser._normalize(namespaced_element)
 
         self.assertEqual(normalized_uri, uri,
                 'should be equal') and \
