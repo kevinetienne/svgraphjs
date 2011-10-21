41fdc906d1e72482aea106835f7d7baecb06a24e
ekevin <etienne.kevin@gmail.com>
2011-10-21 12:47:11 +0100

    added tokens

---
+++ tokens.py
@@ -0,0 +1,8 @@
+# init a js variable
+init_var_with_value = "var %s = %s;"
+# create a rectangle element with raphaeljs
+raphael_rect = "paper.rect(%(x)s,%(y)s,%(width)s,%(height)s)i"
+# create an ellipse element with raphaeljs
+raphael_ellipse = "paper.ellipse(%(cx)s,%(cy)s,%(rx)s,%(ry)s);"
+# define attributes for an element
+raphael_element_attr = "%s.attr({%s});"
